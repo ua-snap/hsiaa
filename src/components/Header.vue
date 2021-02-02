d30556b67779add55630dc51e65dc2c992d16296
Bob Torgerson <rltorgerson@alaska.edu>
2021-02-02 14:21:29 -0900

    Modified ACCAP banner anchor link to go to ACCAP website.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://uaf-iarc.org">
+        <a class="navbar-item" href="https://uaf-accap.org">
           <img src="../assets/ACCAP_wide.svg" />
         </a>
 
