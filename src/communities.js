834b229128083f28b44b1bbb4bfec3a12c57da12
Bruce Crevensten <bruce.crevensten@gmail.com>
2021-02-24 12:48:17 -0800

    Ships can travel!

--- src/communities.js
+++ src/communities.js
@@ -1,96 +1,691 @@
 export default {
-  AK: {
-    name: "Alaska communities",
+  Alaska: {
+    name: "Alaska",
     places: [
-      { place: "62.6851300839551,-165.051458350888", name: "Alakanuk, AK" },
       {
-        place: "65.2839013106643,-166.543276878432",
+        place: "62.69,-165.05",
+        name: "Alakanuk, AK"
+      },
+      {
+        place: "65.27,-166.69",
         name: "Brevig Mission, AK"
       },
-      { place: "54.8928703605601,-162.585689817175", name: "Cold Bay, AK" },
-      { place: "66.215846873033,-162.686018873669", name: "Deering, AK" },
-      { place: "58.6984670397717,-158.604182960483", name: "Dillingham, AK" },
-      { place: "65.7450538858085,-168.982175323199", name: "Diomede, AK" },
-      { place: "58.217525752542,-157.610129237119", name: "Egegik, AK" },
-      { place: "58.808756,-158.569142", name: "Ekuk, AK" },
-      { place: "64.55918135633,-162.105188401375", name: "Elim, AK" },
-      { place: "62.8222221972983,-164.936452924562", name: "Emmonak, AK" },
-      { place: "64.4814106580503,-165.422359375284", name: "Fort Davis, AK" },
-      { place: "63.8090755288085,-171.755535579505", name: "Gambell, AK" },
-      { place: "64.3832176116423,-162.956763792118", name: "Golovin, AK" },
-      { place: "59.1729106218548,-161.775521544658", name: "Goodnews Bay, AK" },
-      { place: "61.5093172991055,-166.002724660499", name: "Hooper Bay, AK" },
-      { place: "70.1927932513101,-143.639697294833", name: "Kaktovik, AK" },
-      { place: "60.1052439609978,-164.479848278604", name: "Kipnuk, AK" },
-      { place: "67.6417368319659,-164.673004082018", name: "Kivalina, AK" },
-      { place: "59.9141493639226,-162.893349156378", name: "Kongiganak, AK" },
-      { place: "63.1143882756083,-163.412739594096", name: "Kotlik, AK" },
-      { place: "66.8287674399298,-162.784051401057", name: "Kotzebue, AK" },
-      { place: "64.7047630607052,-161.161760498773", name: "Koyuk, AK" },
-      { place: "59.8441824426393,-163.117809247724", name: "Kwigillingok, AK" },
-      { place: "60.5077800612199,-166.277527564528", name: "Mekoryuk, AK" },
-      { place: "58.6516550698052,-157.466986930608", name: "Naknek, AK" },
       {
-        place: "56.0832824650499,-161.222756583751",
+        place: "54.89,-162.59",
+        name: "Cold Bay, AK"
+      },
+      {
+        place: "66.22,-162.69",
+        name: "Deering, AK"
+      },
+      {
+        place: "58.7,-158.6",
+        name: "Dillingham, AK"
+      },
+      {
+        place: "65.75,-168.98",
+        name: "Diomede, AK"
+      },
+      {
+        place: "58.22,-157.61",
+        name: "Egegik, AK"
+      },
+      {
+        place: "58.7,-158.6",
+        name: "Ekuk, AK"
+      },
+      {
+        place: "64.56,-162.11",
+        name: "Elim, AK"
+      },
+      {
+        place: "62.82,-164.94",
+        name: "Emmonak, AK"
+      },
+      {
+        place: "64.48,-165.42",
+        name: "Fort Davis, AK"
+      },
+      {
+        place: "63.81,-171.76",
+        name: "Gambell, AK"
+      },
+      {
+        place: "64.38,-162.96",
+        name: "Golovin, AK"
+      },
+      {
+        place: "59.16,-161.83",
+        name: "Goodnews Bay, AK"
+      },
+      {
+        place: "61.51,-166",
+        name: "Hooper Bay, AK"
+      },
+      {
+        place: "70.19,-143.64",
+        name: "Kaktovik, AK"
+      },
+      {
+        place: "59.9,-164.44",
+        name: "Kipnuk, AK"
+      },
+      {
+        place: "67.64,-164.67",
+        name: "Kivalina, AK"
+      },
+      {
+        place: "59.91,-162.89",
+        name: "Kongiganak, AK"
+      },
+      {
+        place: "63.11,-163.41",
+        name: "Kotlik, AK"
+      },
+      {
+        place: "66.83,-162.78",
+        name: "Kotzebue, AK"
+      },
+      {
+        place: "64.7,-161.16",
+        name: "Koyuk, AK"
+      },
+      {
+        place: "59.84,-163.12",
+        name: "Kwigillingok, AK"
+      },
+      {
+        place: "60.63,-166.27",
+        name: "Mekoryuk, AK"
+      },
+      {
+        place: "58.65,-157.47",
+        name: "Naknek, AK"
+      },
+      {
+        place: "56.08,-161.22",
         name: "Nelson Lagoon, AK"
       },
-      { place: "60.8843376204438,-165.15993447947", name: "Newtok, AK" },
-      { place: "64.4814106580503,-165.452359375284", name: "Nome, AK" },
-      { place: "62.5790920702393,-165.204312655182", name: "Nunam Iqua, AK" },
-      { place: "57.59939366239,-157.833296426434", name: "Pilot Point, AK" },
-      { place: "58.9871442717979,-161.950343490552", name: "Platinum, AK" },
-      { place: "68.2884307616777,-166.876912959572", name: "Point Hope, AK" },
-      { place: "69.7262618857318,-163.385572237574", name: "Point Lay, AK" },
-      { place: "56.959948218085,-158.825768650723", name: "Port Heiden, AK" },
-      { place: "70.4487179863218,-148.033321138673", name: "Prudhoe Bay, AK" },
-      { place: "59.7347453630506,-162.003149760407", name: "Quinhagak, AK" },
-      { place: "63.7325806762895,-169.470088541689", name: "Savoonga, AK" },
-      { place: "61.8728846470491,-165.733313425583", name: "Scammon Bay, AK" },
-      { place: "64.2601704074258,-161.45302011971", name: "Shaktoolik, AK" },
-      { place: "66.217626405847,-166.227577594336", name: "Shishmaref, AK" },
-      { place: "56.6332792236121,-169.530826798787", name: "St. George, AK" },
-      { place: "63.5982681460881,-161.854029767968", name: "St. Michael, AK" },
-      { place: "57.1088351477948,-170.254768664541", name: "St. Paul, AK" },
-      { place: "63.5054317849543,-162.410429528882", name: "Stebbins, AK" },
-      { place: "65.2839013106643,-166.543276878432", name: "Teller, AK" },
-      { place: "58.9422727621214,-160.438188485022", name: "Togiak, AK" },
-      { place: "60.4958384778163,-165.112175247689", name: "Toksook Bay, AK" },
-      { place: "60.6948225298798,-165.356563870896", name: "Tununak, AK" },
-      { place: "60.4958384778163,-165.112175247689", name: "Umkumiut, AK" },
-      { place: "63.8501941017296,-161.260957590821", name: "Unalakteet, AK" },
-      { place: "71.4713063075264,-156.665176932233", name: "Utqiaġvik, AK" },
-      { place: "70.787461075763,-160.265186401037", name: "Wainwright, AK" },
-      { place: "65.5785629514697,-168.28334108969", name: "Wales, AK" }
+      {
+        place: "60.88,-165.16",
+        name: "Newtok, AK"
+      },
+      {
+        place: "64.48,-165.42",
+        name: "Nome, AK"
+      },
+      {
+        place: "62.58,-165.2",
+        name: "Nunam Iqua, AK"
+      },
+      {
+        place: "57.6,-157.83",
+        name: "Pilot Point, AK"
+      },
+      {
+        place: "58.99,-161.95",
+        name: "Platinum, AK"
+      },
+      {
+        place: "68.29,-166.88",
+        name: "Point Hope, AK"
+      },
+      {
+        place: "69.73,-163.39",
+        name: "Point Lay, AK"
+      },
+      {
+        place: "57.01,-158.95",
+        name: "Port Heiden, AK"
+      },
+      {
+        place: "70.45,-148.03",
+        name: "Prudhoe Bay, AK"
+      },
+      {
+        place: "59.72,-162.11",
+        name: "Quinhagak, AK"
+      },
+      {
+        place: "64.06,-170.51",
+        name: "Savoonga, AK"
+      },
+      {
+        place: "61.94,-165.92",
+        name: "Scammon Bay, AK"
+      },
+      {
+        place: "64.26,-161.45",
+        name: "Shaktoolik, AK"
+      },
+      {
+        place: "66.22,-166.23",
+        name: "Shishmaref, AK"
+      },
+      {
+        place: "56.63,-169.53",
+        name: "St. George, AK"
+      },
+      {
+        place: "63.81,-161.94",
+        name: "St. Michael, AK"
+      },
+      {
+        place: "57.11,-170.25",
+        name: "St. Paul, AK"
+      },
+      {
+        place: "63.46,-162.74",
+        name: "Stebbins, AK"
+      },
+      {
+        place: "65.23,-166.81",
+        name: "Teller, AK"
+      },
+      {
+        place: "58.89,-160.47",
+        name: "Togiak, AK"
+      },
+      {
+        place: "60.5,-165.11",
+        name: "Toksook Bay, AK"
+      },
+      {
+        place: "60.83,-165.38",
+        name: "Tununak, AK"
+      },
+      {
+        place: "60.5,-165.11",
+        name: "Umkumiut, AK"
+      },
+      {
+        place: "63.85,-161.26",
+        name: "Unalakteet, AK"
+      },
+      {
+        place: "71.47,-156.67",
+        name: "Utqiaġvik, AK"
+      },
+      {
+        place: "70.79,-160.27",
+        name: "Wainwright, AK"
+      },
+      {
+        place: "65.58,-168.28",
+        name: "Wales, AK"
+      }
     ]
   },
-  NWT: {
-    name: "North West Territory communities",
+  "Northwest Territories": {
+    name: "Northwest Territories",
     places: [
-      { place: "69.9610863947203,-123.661517656453", name: "Paultuak, NWT" },
       {
-        place: "71.8632712337163,-125.482112134379",
+        place: "69.96,-123.66",
+        name: "Paulatuk, NWT"
+      },
+      {
+        place: "71.86,-125.48",
         name: "Sachs Harbour, NWT"
       },
-      { place: "69.6835584167738,-133.371989420137", name: "Tuktoyaktuk, NWT" }
+      {
+        place: "69.68,-133.37",
+        name: "Tuktoyaktuk, NWT"
+      },
+      {
+        place: "70.62,-118.32",
+        name: "Ulukhaktok, NWT"
+      }
     ]
   },
-  RU: {
-    name: "Russian communities",
+  Nunavut: {
+    name: "Nunavut",
     places: [
-      { place: "67.8909773649486,-175.614923904914", name: "Ванкарем, RU" },
-      { place: "66.3529324921101,-170.27006503675", name: "Инчоун, RU" },
-      { place: "65.8932829095358,-178.875266395884", name: "Конергино, RU" },
-      { place: "65.3616386823124,-170.303365741367", name: "Лаврентия, RU" },
-      { place: "65.4372201267222,-171.663454294326", name: "Лорино, RU" },
-      { place: "67.0633662935393,-172.870441076511", name: "Нешкан, RU" },
-      { place: "64.4275690840349,-172.14258985466", name: "Новое Чаплино, RU" },
-      { place: "64.7547774117121,-175.486856576098", name: "Нунлигран, RU" },
-      { place: "67.5528205102073,-174.700141908481", name: "Нутэпэльмен, RU" },
-      { place: "64.3030720574871,-174.094925268185", name: "Сиреники, RU" },
-      { place: "66.2173014811322,-169.734038988469", name: "Уэлен, RU" },
-      { place: "65.0015999276741,-175.911858185307", name: "Энмелен, RU" },
-      { place: "66.9733669397493,-171.808380256471", name: "Энурмино, RU" },
-      { place: "64.88179603077,-172.345075328797", name: "Янракыннот, RU" }
+      {
+        place: "82.51,-61.22",
+        name: "Alert, NU"
+      },
+      {
+        place: "73.15,-85.52",
+        name: "Arctic Bay, NU"
+      },
+      {
+        place: "61.08,-93.89",
+        name: "Arviat, NU"
+      },
+      {
+        place: "68.96,-105.38",
+        name: "Cambridge Bay, NU"
+      },
+      {
+        place: "64.08,-76.48",
+        name: "Cape Dorset, NU"
+      },
+      {
+        place: "63.40,-90.65",
+        name: "Chesterfield Inlet, NU"
+      },
+      {
+        place: "70.31,-68.48",
+        name: "Clyde River, NU"
+      },
+      {
+        place: "63.96,-82.83",
+        name: "Coral Harbour, NU"
+      },
+      {
+        place: "68.52,-95.92",
+        name: "Gjoa Haven, NU"
+      },
+      {
+        place: "76.44,-83.15",
+        name: "Grise Fjord, NU"
+      },
+      {
+        place: "68.78,-81.05",
+        name: "Hall Beach, NU"
+      },
+      {
+        place: "69.30,-81.53",
+        name: "Igloolik, NU"
+      },
+      {
+        place: "63.64,-68.45",
+        name: "Iqaluit, NU"
+      },
+      {
+        place: "62.67,-69.98",
+        name: "Kimmirut, NU"
+      },
+      {
+        place: "68.50,-90.12",
+        name: "Kugaaruk, NU"
+      },
+      {
+        place: "68.03,-113.83",
+        name: "Kugluktuk, NU"
+      },
+      {
+        place: "66.40,-85.55",
+        name: "Naujaat, NU"
+      },
+      {
+        place: "66.05,-66.25",
+        name: "Pangnirtung, NU"
+      },
+      {
+        place: "72.77,-76.29",
+        name: "Pond Inlet, NU"
+      },
+      {
+        place: "67.37,-63.76",
+        name: "Qikiqtarjuaq, NU"
+      },
+      {
+        place: "62.71,-91.62",
+        name: "Rankin Inlet, NU"
+      },
+      {
+        place: "74.62,-95.18",
+        name: "Resolute, NU"
+      },
+      {
+        place: "56.47,-78.90",
+        name: "Sanikiluaq, NU"
+      },
+      {
+        place: "69.48,-94.07",
+        name: "Taloyoak, NU"
+      },
+      {
+        place: "62.14,-92.56",
+        name: "Whale Cove, NU"
+      }
+    ]
+  },
+  Manitoba: {
+    name: "Manitoba",
+    places: [
+      {
+        place: "58.79,-93.94",
+        name: "Churchill, MB"
+      }
+    ]
+  },
+  Ontario: {
+    name: "Ontario",
+    places: [
+      {
+        place: "52.96,-82.26",
+        name: "Attawapiskat, ON"
+      },
+      {
+        place: "52.22,-81.39",
+        name: "Fort Albany, ON"
+      },
+      {
+        place: "56.02,-87.63",
+        name: "Fort Severn, ON"
+      },
+      {
+        place: "52.37,-81.38",
+        name: "Kashechewan, ON"
+      },
+      {
+        place: "51.37,-80.31",
+        name: "Moosonee, ON"
+      }
+    ]
+  },
+  Quebec: {
+    name: "Quebec",
+    places: [
+      {
+        place: "60.86,-78.3",
+        name: "Akulivik, QC"
+      },
+      {
+        place: "59.4,-69.61",
+        name: "Aupaluk, QC"
+      },
+      {
+        place: "53.82,-79.15",
+        name: "Chisasibi, QC"
+      },
+      {
+        place: "52.23,-78.62",
+        name: "Eastmain, QC"
+      },
+      {
+        place: "58.38,-78.33",
+        name: "Inukjuak, QC"
+      },
+      {
+        place: "62.41,-78.07",
+        name: "Ivujivik, QC"
+      },
+      {
+        place: "58.82,-66.1",
+        name: "Kangiqsualujjuaq, QC"
+      },
+      {
+        place: "61.72,-71.94",
+        name: "Kangiqsujuaq, QC"
+      },
+      {
+        place: "60,-69.46",
+        name: "Kangirsuk, QC"
+      },
+      {
+        place: "58.51,-68.08",
+        name: "Kuujjuaq, QC"
+      },
+      {
+        place: "55.32,-77.73",
+        name: "Kuujjuarapik, QC"
+      },
+      {
+        place: "59.96,-77.74",
+        name: "Puvirnituq, QC"
+      },
+      {
+        place: "60.91,-69.39",
+        name: "Quaqtaq, QC"
+      },
+      {
+        place: "62.31,-75.76",
+        name: "Salluit, QC"
+      },
+      {
+        place: "58.78,-69.77",
+        name: "Tasiujaq, QC"
+      },
+      {
+        place: "56.5,-76.71",
+        name: "Umiujaq, QC"
+      },
+      {
+        place: "51.49,-78.94",
+        name: "Waskaganish, QC"
+      },
+      {
+        place: "52.97,-78.98",
+        name: "Wemindji, QC"
+      },
+      {
+        place: "55.32,-77.73",
+        name: "Whapmagoostui, QC"
+      }
+    ]
+  },
+  Labrador: {
+    name: "Labrador",
+    places: [
+      {
+        place: "53.47,-55.72",
+        name: "Black Tickle, NL"
+      },
+      {
+        place: "51.54,-56.72",
+        name: "Capstan Island, NL"
+      },
+      {
+        place: "53.8,-57.02",
+        name: "Cartwright, NL"
+      },
+      {
+        place: "52.71,-55.88",
+        name: "Charlottetown, NL"
+      },
+      {
+        place: "51.42,-56.89",
+        name: "English Point, NL"
+      },
+      {
+        place: "55.3,-60.09",
+        name: "Hopedale, NL"
+      },
+      {
+        place: "51.37,-57.04",
+        name: "L'Anse-au-Clair"
+      },
+      {
+        place: "51.49,-56.78",
+        name: "L'Anse-au-Loup"
+      },
+      {
+        place: "52.2,-55.7",
+        name: "Lodge Bay, NL"
+      },
+      {
+        place: "54.93,-58.93",
+        name: "Makkovik, NL"
+      },
+      {
+        place: "52.33,-55.71",
+        name: "Mary's Harbour, NL"
+      },
+      {
+        place: "56.54,-61.26",
+        name: "Nain, NL"
+      },
+      {
+        place: "56.03,-61.19",
+        name: "Natuashish, NL"
+      },
+      {
+        place: "53.53,-57.29",
+        name: "Paradise River, NL"
+      },
+      {
+        place: "52.82,-55.75",
+        name: "Pinsent's Arm, NL"
+      },
+      {
+        place: "51.59,-56.61",
+        name: "Pinware, NL"
+      },
+      {
+        place: "55,-59.7",
+        name: "Postville, NL"
+      },
+      {
+        place: "51.71,-56.41",
+        name: "Red Bay, NL"
+      },
+      {
+        place: "54.33,-57.83",
+        name: "Rigolet, NL"
+      },
+      {
+        place: "51.58,-56.67",
+        name: "West Saint Modeste, NL"
+      }
+    ]
+  },
+  Newfoundland: {
+    name: "Newfoundland",
+    places: [
+      {
+        place: "51.34,-55.38",
+        name: "St. Anthony, NL"
+      },
+      {
+        place: "50.67,-55.93",
+        name: "Englee, NL"
+      },
+      {
+        place: "50.14,-55.79",
+        name: "Baie Verte, NL"
+      },
+      {
+        place: "49.86,-55.01",
+        name: "Crow Head, NL"
+      },
+      {
+        place: "49.87,-54.57",
+        name: "Twilingate, NL"
+      },
+      {
+        place: "49.47,-53.37",
+        name: "Deadman's Bay, NL"
+      },
+      {
+        place: "48.82,-53.41",
+        name: "Eastport, NL"
+      },
+      {
+        place: "48.73,-52.96",
+        name: "Bonavista, NL"
+      },
+      {
+        place: "48.23,-52.85",
+        name: "Grates Cove, NL"
+      },
+      {
+        place: "47.8,-52.73",
+        name: "Pouch Cove, NL"
+      },
+      {
+        place: "47.6,-52.54",
+        name: "St. Johns, NL"
+      }
+    ]
+  },
+  Chukotka: {
+    name: "Chukotka",
+    places: [
+      {
+        place: "67.89,-175.61",
+        name: "Ванкарем, CAR"
+      },
+      {
+        place: "66.32,-170.27",
+        name: "Инчоун, CAR"
+      },
+      {
+        place: "65.89,-178.88",
+        name: "Конергино, CAR"
+      },
+      {
+        place: "65.36,-170.3",
+        name: "Лаврентия, CAR"
+      },
+      {
+        place: "65.44,-171.66",
+        name: "Лорино, CAR"
+      },
+      {
+        place: "67.22,-172.89",
+        name: "Нешкан, CAR"
+      },
+      {
+        place: "64.43,-172.14",
+        name: "Новое Чаплино, CAR"
+      },
+      {
+        place: "64.73,-175.59",
+        name: "Нунлигран, CAR"
+      },
+      {
+        place: "67.55,-174.7",
+        name: "Нутэпэльмен, CAR"
+      },
+      {
+        place: "64.3,-174.09",
+        name: "Сиреники, CAR"
+      },
+      {
+        place: "66.22,-169.73",
+        name: "Уэлен, CAR"
+      },
+      {
+        place: "65,-175.91",
+        name: "Энмелен, CAR"
+      },
+      {
+        place: "66.97,-171.81",
+        name: "Энурмино, CAR"
+      },
+      {
+        place: "64.88,-172.35",
+        name: "Янракыннот, CAR"
+      },
+      {
+        place: "64.43,178.72",
+        name: "Анадырь, CAR"
+      },
+      {
+        place: "63,179.78",
+        name: "Беринговский, CAR"
+      },
+      {
+        place: "69.96,167.92",
+        name: "Айон, CAR"
+      },
+      {
+        place: "69.03,170.83",
+        name: "Красноарме́йский, CAR"
+      },
+      {
+        place: "69.83,169.95",
+        name: "Валькумей, CAR"
+      },
+      {
+        place: "69.83,169.95",
+        name: "Певек, CAR"
+      },
+      {
+        place: "69.55,178.68",
+        name: "Ленинградский, CAR"
+      },
+      {
+        place: "68.93,-179.12",
+        name: "Мыс Шмидта, CAR"
+      },
+      {
+        place: "68.93,-179.54",
+        name: "Рыркайпий, CAR"
+      }
     ]
   }
 };
