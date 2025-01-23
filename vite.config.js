513cb65300e9723da5b328067d1880d10ff5f945
Craig Stephenson <crstephenson@alaska.edu>
2025-01-23 12:17:55 -0900

    Remove URL subpath from production publicPath.

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === "production" ? "/tools/sea-ice-atlas/" : "/",
+  base: "/",
 })
